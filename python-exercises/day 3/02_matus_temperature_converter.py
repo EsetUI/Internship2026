ace7472af681958a0006eb3512177a6bda232490
Matúš Mačuga <mmacuga176@gmail.com>
2026-05-13 12:27:11 +0200

    added the format to the other strings

--- python-exercises/day 3/02_matus_temperature_converter.py
+++ python-exercises/day 3/02_matus_temperature_converter.py
@@ -1,15 +1,15 @@
-lang = input("What language do you want to use this converter in? (sk or en): ")
+lang = input(f"What language do you want to use this converter in? (sk or en): ")
 
 
 
 languages = {
     "sk" : {
-        'choice': "Bola vybratá slovenčina",
+        'choice': "Bola vybraná slovenčina",
         'conversion': "Chceš premeniť jednotky ZO stupňov Celzia na stupne fahrenheit (1 pre celsius, 2 pre fahrenheit): ",
         'inputc': "Zadaj teplou (°C): ",
         'inputf': "Zadaj teplotu (F): ",
-        'c_to_f': "Teplota premenená do Fahrenheitov je ",
-        'f_to_c': "Teplota premenená do stupňov Celzia je ",
+        'c_to_f': "Teplota premenená do Fahrenheitov je {} F",
+        'f_to_c': "Teplota premenená do stupňov Celzia je {} °C",
         'error': "Prepáč, skús to znova (1 alebo 2)"
     },
 
@@ -18,41 +18,45 @@ languages = {
         'conversion': "Do you want to convert FROM celsius or fahrenheit (1 for celsius, 2 for fahrenheit): ",
         'inputc': "The temperature is (°C): ",
         'inputf': "The temperature is (F): ",
-        'c_to_f': "The temperature converted to F is ",
-        'f_to_c': "The temperature converted to °C is ",
-        'error': "Sorry, try again (1 or 2)"
+        'c_to_f': "The temperature converted to F is {} F",
+        'f_to_c': "The temperature converted to °C is {} °C",
+        'error': "Sorry, try again (1 or 2)",
+        'vluerr': "Sorry, an error occured, try again"
     }
 }
-
 
 
 text = languages[lang]
 
 
-
-
 print(text['choice'])
 
 
-
+print("---------------------------------------------------")
 
 
 
 choose = input(text['conversion'])
+print("---------------------------------------------------")
 
 if choose == "1":
 
     celsius = float(input(text['inputc']))
     fahrenheit = celsius * 1.8 + 32
-    print(f"{text['c_to_f']}{fahrenheit}")
-
+    print(text['c_to_f'].format(fahrenheit))
 
 elif choose == "2":
 
     fahrenheit = float(input(text['inputf']))
-    celsius = fahrenheit /1.8 - 32
-    print(f"{text['f_to_c']}{celsius}")
+    celsius = (fahrenheit-32) /1.8
+    print(text['f_to_c'].format(celsius))
 
 
 else:
-    print(text['error'])
+     print(text['error'])
+        
+
+
+
+
+
