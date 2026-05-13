18717b02ff170d8c2f6f0e1816cfe2c94deac5d1
PeterAdamec <peter_adamec@icloud.com>
2026-05-13 15:24:18 +0200

    all input checkes added

--- cpp-exercises/day3/main.cpp
+++ cpp-exercises/day3/main.cpp
@@ -8,40 +8,39 @@ double f_to_c(double);
 double c_to_k(double);
 double k_to_c(double);
 void print_output(std::span<std::string_view>, double );
-double request_valid_input(std::string_view);
+
+template<typename T>
+T request_valid_input(std::string_view,std::string_view error_msg);
+
+template<typename T, typename _T>
+T request_valid_input(std::string_view , std::string_view , _T);
+
 
 int main() {
 	double degrees{};
-	char lang{};
 	std::string_view slovak[] = { {"Zadajte teplotu na konverciu ({} na {}): "},{"vasa teplota vo {} je {}{}"}, {"Zadajte z akej jednotky chcete konvertovat: "},
-								{"Zadajte do akej jednotky chcete konvertovat"},{"Moznosti:\nC = celzia\nF = farengeit\nK = kelvin\n"},};
+								{"Zadajte do akej jednotky chcete konvertovat: "},{"Moznosti:\nC = celzia\nF = farengeit\nK = kelvin\n"},};
 	std::string_view english[] = { {"Enter a temperature to convert ({} to {}): "}, {"Your temperature in {} is {}{}"}, {"Enter from which unit you want to convert: "},
 								{"Enter to which unit you want to convert: "},{"Options:\nC = celsius\nF = fahrenheit\nK = kelvin\n"},};
 	
-	std::cout << "s = slovak\ne = english\nEnter language: ";
-	std::cin >> lang;
+	char lang = request_valid_input<char, std::string>("\ns = slovak\ne = english\nEnter language: ", "Invalid language please only enter from the language options\n\n", "se");
 
 	switch (lang) {
 	case 's':
 		print_output(slovak, degrees);
 		break;
-	default:
+	case 'e':
 		print_output(english, degrees);
+		break;
 	}
-
 }
 
 void print_output(std::span<std::string_view> arr, double degrees) {
-	char unit_from;
-	char unit_to;
 	std::cout << arr[4];
-	std::cout << arr[2];
-	std::cin >> unit_from;
-
-	std::cout << arr[3];
-	std::cin >> unit_to;
+	char unit_from = request_valid_input<char, std::string>(arr[2], "\nInvalid input you have to enter a valid unit(C,F,K)\n\n","CFK");
+	char unit_to = request_valid_input<char, std::string>(arr[3], "\nInvalid input you have to enter a valid unit(C,F,K)\n\n","CFK");
 	
-	degrees = request_valid_input(std::vformat(arr[0],std::make_format_args(unit_from, unit_to)));
+	degrees = request_valid_input<double>(std::vformat(arr[0],std::make_format_args(unit_from, unit_to)), "\nInvalid input enter a real number\n\n");
 
 	double temp{};
 	switch (unit_from) {
@@ -94,13 +93,14 @@ double k_to_c(double k) {
 	return k - 273.15;
 }
 
-double request_valid_input(std::string_view input_msg) {
-	double degrees{};
+template<typename T>
+T request_valid_input(std::string_view input_msg, std::string_view error_msg) {
+	T value{};
 	std::string input;
 	while (true) {
 		std::cout << input_msg;
-		if (!(std::cin >> degrees)) {
-			std::cerr << "\nWrong input try a real number\n\n";
+		if (!(std::cin >> value)) {
+			std::cerr << error_msg;
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			continue;
@@ -110,5 +110,29 @@ double request_valid_input(std::string_view input_msg) {
 		}
 	}
 
-	return degrees;
+	return value;
+}
+
+template<typename T, typename _T>
+T request_valid_input(std::string_view input_msg, std::string_view error_msg, _T input_options) {
+	T value{};
+	std::string input;
+	while (true) {
+		std::cout << input_msg;
+		if (!(std::cin >> value)) {
+			std::cerr << error_msg;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		else {
+			for (auto option : input_options) {
+				if (option == value) return value;
+			}
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << error_msg;
+			return request_valid_input<T, _T>(input_msg, error_msg, input_options);
+		}
+	}
 }
