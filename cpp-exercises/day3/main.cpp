1c04118c90f2ff443c94682f534a273637cf3aa2
PeterAdamec <peter_adamec@icloud.com>
2026-05-13 09:53:58 +0200

    Language choice feature added

--- cpp-exercises/day3/main.cpp
+++ cpp-exercises/day3/main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
-using namespace std;
-
+#include <format>
+#include <vector>
 double c_to_f(double);
 
 int main() {
+	const size_t number_of_sentences = 2;
 	double degrees;
-	cout << "Enter a temperature to convert (celsius to fahrenheit): ";
-	cin >> degrees;
+	char lang;
+	std::string slovak[] = { {"Zadajte teplotu na konverciu (celzia na farenheit): "},{"vasa teplota vo farenheitoch je "} };
+	std::string english[] = { {"Enter a temperature to convert (celsius to fahrenheit): "}, {"Your temperature in fahrenheit is "} };
+	
+	std::string language[number_of_sentences];
 
-	cout << "Your temperature in fahrenheit is " << c_to_f(degrees) << " degrees\n";
+	std::cout << "s = slovak\ne = english\nEnter language: ";
+	std::cin >> lang;
+	switch (lang){
+	case 's':
+		for (int i = 0;i < number_of_sentences;i++) {
+			language[i] = slovak[i];
+		}
+		break;
+	default:
+		for (int i = 0;i < number_of_sentences;i++) {
+			language[i] = english[i];
+		}
+	}
+
+	std::cout << std::format("{}", language[0]);
+	std::cin >> degrees;
+	std::cout << std::format("{}{}F", language[1], c_to_f(degrees));
 }
 
 double c_to_f(double c) {
