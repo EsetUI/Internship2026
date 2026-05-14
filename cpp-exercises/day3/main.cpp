92b97550e4c526794da2014f08dbc26cd1ed5ed5
Matúš Mačuga <mmacuga176@gmail.com>
2026-05-14 13:38:09 +0200

    added avg, count, min, max

---
+++ cpp-exercises/day3/main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <format>
+#include <span>
+#include <string>
+
+double c_to_f(double);
+double f_to_c(double);
+double c_to_k(double);
+double k_to_c(double);
+void print_output(std::span<std::string_view>, double );
+
+template<typename T>
+T request_valid_input(std::string_view,std::string_view error_msg);
+
+template<typename T, typename _T>
+T request_valid_input(std::string_view , std::string_view ,const _T&);
+
+
+int main() {
+	double degrees{};
+	std::string_view slovak[] = { {"Zadajte teplotu na konverciu ({} na {}): "},{"vasa teplota vo {} je {}{}"}, {"Zadajte z akej jednotky chcete konvertovat: "},
+								{"Zadajte do akej jednotky chcete konvertovat: "},{"Moznosti:\nC = celzia\nF = farengeit\nK = kelvin\n"},};
+	std::string_view english[] = { {"Enter a temperature to convert ({} to {}): "}, {"Your temperature in {} is {}{}"}, {"Enter from which unit you want to convert: "},
+								{"Enter to which unit you want to convert: "},{"Options:\nC = celsius\nF = fahrenheit\nK = kelvin\n"},};
+	
+	char lang = request_valid_input<char, std::string>("\ns = slovak\ne = english\nEnter language: ", "Invalid language please only enter from the language options\n\n", "se");
+
+	switch (lang) {
+	case 's':
+		print_output(slovak, degrees);
+		break;
+	case 'e':
+		print_output(english, degrees);
+		break;
+	}
+}
+
+void print_output(std::span<std::string_view> arr, double degrees) {
+	std::cout << arr[4];
+	char unit_from = request_valid_input<char, std::string>(arr[2], "\nInvalid input you have to enter a valid unit(C,F,K)\n\n","CFK");
+	char unit_to = request_valid_input<char, std::string>(arr[3], "\nInvalid input you have to enter a valid unit(C,F,K)\n\n","CFK");
+	
+	degrees = request_valid_input<double>(std::vformat(arr[0],std::make_format_args(unit_from, unit_to)), "\nInvalid input enter a real number\n\n");
+
+	double temp{};
+	switch (unit_from) {
+	case 'C':
+		switch (unit_to) {
+		case 'F': 
+			temp = c_to_f(degrees); 
+			break;
+		case 'K': 
+			temp = c_to_k(degrees); 
+			break;
+		}
+		break;
+	case 'F':
+		switch (unit_to) {
+		case 'C': 
+			temp = f_to_c(degrees);
+			break;
+		case 'K':
+			temp = f_to_c(degrees); 
+			temp = c_to_k(temp);
+			break;
+		}
+		break;
+	case 'K':
+		switch (unit_to) {
+		case 'C':
+			temp = k_to_c(degrees);
+			break;
+		case 'F':
+			temp = k_to_c(degrees);
+			temp = c_to_f(temp);
+			break;
+		}
+		break;
+	}
+	std::cout << std::vformat(arr[1], std::make_format_args(unit_to, temp, unit_to));
+}
+
+double c_to_f(double c) {
+	return c * 9.0 / 5.0 + 32.0;
+}
+double f_to_c(double f) {
+	return (f - 32) * 5 / 9;
+}
+double c_to_k(double c) {
+	return c + 273.15;
+}
+double k_to_c(double k) {
+	return k - 273.15;
+}
+
+template<typename T>
+T request_valid_input(std::string_view input_msg, std::string_view error_msg) {
+	T value{};
+	std::string input;
+	while (true) {
+		std::cout << input_msg;
+		if (!(std::cin >> value)) {
+			std::cerr << error_msg;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		else {
+			break;
+		}
+	}
+
+	return value;
+}
+
+template<typename T, typename _T>
+T request_valid_input(std::string_view input_msg, std::string_view error_msg,const _T &input_options) {
+	T value{};
+	std::string input;
+	while (true) {
+		std::cout << input_msg;
+		if (!(std::cin >> value)) {
+			std::cerr << error_msg;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		else {
+			for (auto option : input_options) {
+				if (option == value) return value;
+			}
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << error_msg;
+			return request_valid_input<T, _T>(input_msg, error_msg, input_options);
+		}
+	}
+}
--- python-exercises/day 4/03_matus_number_stats.py
+++ python-exercises/day 4/03_matus_number_stats.py
@@ -1,5 +1,9 @@
 
-def parse_numbers(text: str)-> list[float]:
+
+
+import argparse
+
+def parse_numbers(text: str) -> list[float]:
     normalized = text.replace(";", ",")
     return [float(i.strip()) for i in normalized.split(",") if i.strip()]
     # return [float(i.strip()) for i in str.split(',;') if i.strip()]         nefunguje
@@ -10,19 +14,51 @@ def num_desc(numbers: list[float]) -> dict[str, float]:
     return {"sum": sumnum}
 
 
-
 def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="MatusNumberStats",
+        description="provides sum of numbers",
+        epilog="Text at the bottom of help",
+    )
+    parser.add_argument("filename", nargs="?")
+    args = parser.parse_args()
+
+    print(args.filename)
     name = input("Name: ")
     print(f"Hello {name}, welcome!")
     print("This is a program called number stats")
+    print("ENJOY")
 
-    numberss = input("Numbers (separated by a coma (, ;)): ")
+    if args.filename:
+        with open(args.filename, "r") as sub:
+            numnum = sub.read().replace(";", ",").split(",")
+        
 
-    numbe = parse_numbers(numberss)
-    endstat = num_desc(numbe)
+        num_list = [float(line.strip()) for line in numnum if line.strip()]
+        numholder = sum(num_list)
 
-    print(f"Sum of the numbers used is: {endstat["sum"]}")
+        print(f"Sum of the numbers used in your text file is: {numholder}")
+        print(f"The number count in your file is: {len(num_list)}")
+        print(f"\033[31mThe min number in your file is: {min(num_list)}\033[0m")
+        print(f"\033[32The max number in your file is: {max(num_list)}\033[0m")
+        print(f"The average number in your file is: {numholder / len(num_list)}")
+
+    else:
+        try:
+            numnum = input("Numbers (separated by a coma (, ;)): ")
+            numbe = parse_numbers(numnum)
+            endstat = num_desc(numbe)
+            
+            print(f"Sum of the numbers used is: {endstat['sum']}")
+            print(f"The number count is: {len(numbe)}")
+            print(f"\033[31mThe min number is: {min(numbe)}\033[0m")
+            print(f"\033[32mThe max number is: {max(numbe)}\033[0m")
+            print(f"The average number is: {endstat['sum'] / len(numbe)}")
+            
+        except ValueError:
+            print("Sorry, wrong input try again!")
 
 
 if __name__ == "__main__":
     main()
+
